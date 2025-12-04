396eea5aef25579d25a33eda4b56f97a29b35e11
Dubrovskirill <zet10set11@gmail.com>
2025-12-04 12:33:00 +0700

    Update main.c

---
+++ lab.2/main.c
@@ -0,0 +1,22 @@
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+
+#define PORT "31337"
+#define MAX_CLIENTS 256
+
+
+int main() {
+
+
+    return 0;
+}
