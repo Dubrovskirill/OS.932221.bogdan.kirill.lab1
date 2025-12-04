fea1ff7f1c5d80cab975ebe305f1a789d266e677
Dubrovskirill <zet10set11@gmail.com>
2025-12-04 12:59:00 +0700

    Update main.c

--- lab.2/main.c
+++ lab.2/main.c
@@ -10,13 +10,45 @@
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/select.h>
 
 #define PORT "31337"
 #define MAX_CLIENTS 256
 
+volatile sig_atomic_t wasSigHup = 0;
+
+
+void sigHupHandler(int sig) {
+    (void)sig; // Подавляем предупреждение о неиспользуемом параметре
+    wasSigHup = 1;
+}
+
+int setup_server_socket();
+void register_signal_handler();
+
 
 int main() {
 
+    int server_socket = setup_server_socket();
+     if (server_socket == -1) {
+        return 1;
+    }
+    int client_sockets[MAX_CLIENTS];
+    int active_clients = 0;
+
+    register_signal_handler();
+    sigset_t blockedMask, origMask;
+    sigemptyset(&blockedMask);
+    sigaddset(&blockedMask, SIGHUP);
+
+     if (sigprocmask(SIG_BLOCK, &blockedMask, &origMask) == -1) {
+        perror("sigprocmask error");
+        close(server_socket);
+        return 1;
+    }
+
+    printf("Server listening on port %s. Waiting for connections and signals...\n", PORT);
+    close(server_socket);
 
     return 0;
 }
