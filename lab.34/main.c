fcbc6f617903550e8a3c0a6a546924cc25402d65
Dubrovskirill <zet10set11@gmail.com>
2025-12-18 19:49:45 +0700

    Update main.c

--- lab.34/main.c
+++ lab.34/main.c
@@ -7,7 +7,7 @@
 #include <linux/version.h>
 #include <linux/string.h>
 #include <linux/jiffies.h>
-
+#include <linux/timekeeping.h> 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("BKS");
 MODULE_DESCRIPTION("TSU Lab Module showing Voyager 2 distance.");
@@ -15,30 +15,29 @@ MODULE_DESCRIPTION("TSU Lab Module showing Voyager 2 distance.");
 #define PROCFS_NAME "tsulab" 
 static struct proc_dir_entry* proc_file = NULL;
 
+
+
 static ssize_t proc_read(struct file* file, char __user* buffer, size_t len, loff_t* offset)
 {
     char message[256]; 
     size_t msg_len;
     
-    
     // Базовое расстояние в км (на момент 15.12.2025)
     long long base_distance_km = 21215499149LL;
 
-    u64 seconds_elapsed = jiffies_to_msecs(jiffies) / 1000;
+    u64 now_seconds = ktime_get_real_seconds();
+    u64 base_seconds = 1765756800; // 15.12.2025
+    u64 seconds_elapsed = (now_seconds > base_seconds) ? (now_seconds - base_seconds) : 0;
     
-    
-    long long distance_increase_km = (long long)seconds_elapsed * 15; 
-    
+    // Прирост (15,4 км/с)
+    long long distance_increase_km = ((long long)seconds_elapsed * 154) / 10;
     long long current_distance_km = base_distance_km + distance_increase_km;
     
     msg_len = scnprintf(message, sizeof(message),
         "Voyager 2 Distance: %lld km\n(Approx. %lld AU)\n", 
         current_distance_km, (current_distance_km / 149597870)); 
-        
 
-    if (*offset >= msg_len) {
-        return 0; 
-    }
+    if (*offset >= msg_len) return 0; 
 
     if (copy_to_user(buffer, message, msg_len)) {
         pr_err("Failed to copy data to user space\n");
